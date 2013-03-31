a05d17b79177ebf9a2d5a8152032b5205dce99da
megroponte <megroponte@gmail.com>
2013-03-31 20:49:02 +0900

    remove the code of anything settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -220,39 +220,6 @@
   (global-set-key (kbd "C-x C-f" 'helm-find-files)
 )
 
-;;; anything
-;;; (auto-install-batch "anything")
-;(when (require 'anything nil t)
-;  (setq
-;   anything-idle-delay 0.3
-;   anything-input-idle-delay 0.2
-;   anything-candidate-number-limit 100
-;   anything-quick-update t
-;   anything-enable-shortcuts 'alphabet))
-
-;; anything global keymap
-;(define-key global-map(kbd "\C-x b") 'anything)
-
-;(when (require 'anything-config nil t)
-;  (setq anything-su-or-sudo "sudo"))
-
-;(require 'anything-match-plugin nil t)
-
-;(when (and (executable-find "cmigemo")
-; 	   (require 'migemo nil t))
-;  (require 'anything-migemo nil t))
-
-;(when (require 'anything-complete nil t)
-;  (anything-lisp-complete-symbol-set-timer 150))
-
-;(require 'anything-show-completion nil t)
-; 
-;(when (require 'auto-install nil t)
-;  (require 'anything-auto-install nil t))
-; 
-;(when (require 'descbinds-anything nil t)
-;  (descbinds-anything-install))
-
 ;;; auto-complete
 (when (require 'auto-complete-config nil t)
   (add-to-list 'ac-dictionary-directories
@@ -276,9 +243,6 @@
 ;;; popwin
 (require 'popwin)
 (setq display-buffer-function 'popwin:display-buffer)
-;; anything
-;(setq anything-samewindow nil)
-;(push '("*anything*" :height 20) popwin:special-display-config)
 ;; M-x dired-jump-other-window
 (require 'dired-x)
 (push '(dired-mode :position top) popwin:special-display-config)
