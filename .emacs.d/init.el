ea9da0fa23db7df407abc7b0f5fffe5aee6a6c5e
megroponte <megroponte@gmail.com>
2013-05-03 20:18:43 +0900

    add showing whitespace & tab

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -52,6 +52,21 @@
   (auto-install-update-emacswiki-package-name t)
   (auto-install-compatibility-setup))
 
+;;; 全角スペース/タブ文字可視化
+(setq whitespace-style
+      '(tabs tab-mark spaces space-mark))
+(setq whitespace-space-regexp "\\(\x3000+\\)")
+(setq whitespace-display-mappings
+      '((space-mark ?\x3000 [?\□])
+        (tab-mark   ?\t   [?\xBB ?\t])
+        ))
+(require 'whitespace)
+(global-whitespace-mode 1)
+(set-face-foreground 'whitespace-space "LightSlateGray")
+(set-face-background 'whitespace-space "DarkSlateGray")
+(set-face-foreground 'whitespace-tab "LightSlateGray")
+(set-face-background 'whitespace-tab "DarkSlateGray")
+
 ;;; 現在行に色をつける
 (global-hl-line-mode 1)
 ;;; 色
