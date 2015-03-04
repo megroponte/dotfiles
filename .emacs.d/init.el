7cb0396c5eab7dec43c41a6ffcb625442d060868
Masayuki Seki <seki@ms-air11-13.local>
2015-03-04 23:27:55 +0900

    modify MaxOSX settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,18 +8,19 @@
 (when (eq system-type 'darwin)
   (require 'ucs-normalize)
   (setq file-name-coding-system 'utf-8-hfs)
-  (setq locale-coding-system 'utf-8-hfs))
+  (setq locale-coding-system 'utf-8-hfs)
 
-;;; キーボードのキー設定 for MacOS X
-(setq mac-option-modifier 'meta) ; OptionキーをMetaキーとしてつかう
-(define-key global-map [165] nil)
-(define-key global-map [67109029] nil)
-(define-key global-map [134217893] nil)
-(define-key global-map [201326757] nil)
-(define-key function-key-map [165] [?\\])
-(define-key function-key-map [67109029] [?\C-\\])
-(define-key function-key-map [134217893] [?\M-\\])
-(define-key function-key-map [201326757] [?\C-\M-\\])
+  ;; キーボードのキー設定
+  (setq mac-option-modifier 'meta) ; OptionキーをMetaキーとしてつかう
+  (define-key global-map [165] nil)
+  (define-key global-map [67109029] nil)
+  (define-key global-map [134217893] nil)
+  (define-key global-map [201326757] nil)
+  (define-key function-key-map [165] [?\\])
+  (define-key function-key-map [67109029] [?\C-\\])
+  (define-key function-key-map [134217893] [?\M-\\])
+  (define-key function-key-map [201326757] [?\C-\M-\\])
+)
 
 ;;; 現在行に色をつける
 (global-hl-line-mode 1)
@@ -141,7 +142,7 @@
 ;;; https://github.com/syohex/emacs-quickrun
 (require 'quickrun)
 (push '("*quickrun*") popwin:special-display-config)
-(global-set-key (kbd "<f5>") 'quickrun)
+(global-set-key (kbd "C-q") 'quickrun)
 
 ;;; magit
 ;;; http://www.magiccircuit.com/emacs-magitメモ
